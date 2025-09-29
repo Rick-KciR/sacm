3c44fe1b249565d6514cf10b621b8844033862e8
Rick-kciR <henriquelira.chsl@gmail.com>
2025-09-29 02:47:30 -0300

    editado os metodos

--- codigo/esquema_simplificado.py
+++ codigo/esquema_simplificado.py
@@ -20,7 +20,7 @@ class Medico:
         self.especialidade = especialidade
     
     def __str__(self):
-        return f"Dr(a):{self.nome}|CRM:{self.crm}|Especialidade:{self.especialidade}"
+        return f"Dr(a):{self.nome}| CRM:{self.crm}| Especialidade:{self.especialidade}"
         
 class Paciente:
     def __init__(self, cpf, nome):
@@ -37,32 +37,34 @@ class Consulta:
         self.especialidade = especialidade
         self.paciente = paciente
         self.dataAgendamento = datetime.now()
-        self.status = False        
+        self.status = "Pendente"        
         
-     
+        
     def agendarConsulta(self):
-        self.status = True
         consultas.append(self) # add objeto 'consulta' atual à lista consulta
-        
-        print("Confirmação de agendamento:")
-        print(f"Agendamento:{self.idConsulta}|paciente:{self.paciente}|Epecialidade:{self.especialidade}" )
+        self.status = "Agendada"
+        print(self)
         
     def cancelarConsulta(self, idConsulta):
-        for consulta in consultas:
-            if consulta.idConsulta == idConsulta:
-                consultas.remove(consulta)
-        print("Consulta cancelada.")
-        self.status = False
+        consulta = self.buscarConsulta(idConsulta)
+        consulta.status = "Cancelada"
+        print(self)
         
-    def buscarConsulta(self):
-        return {
-            "idConsulta": self.idConsulta,
-            }
+    def buscarConsulta(self, idConsulta):
+        for consulta in consultas:
+            if consulta.idConsulta == idConsulta:         
+                return consulta
 
+    def listarCancelada(self):
+        for consulta in consultas:
+            if consulta.status == "Cancelada":
+                return consulta
+        
     def __str__(self):
-        return f"Nº{self.idConsulta}|Paciente:{self.paciente}|Medico:{self.medico}|Data:{self.dataAgendamento}"
+        return f"Nº{self.idConsulta}| Paciente:{self.paciente}| Medico:{self.medico}| Data:{self.dataAgendamento}| Situação: {self.status}"
+
 # Especialidade
-# Criação de especialidades
+# Instâncias de Especialidade
 cardiologista = Especialidade("Cardiologista")
 neurologista = Especialidade("Neurologista")
 pediatra = Especialidade("Pediatra")
@@ -70,47 +72,58 @@ psiquiatra = Especialidade("Psiquiatra")
 oncologista = Especialidade("Oncologista")
 ortopedista = Especialidade("Ortopedista")
 oftamologista = Especialidade("Oftamologista")
-# Médico
-# Criação do médico neurologistao com uma especialidade
-medico1 = Medico("Sophia Henrick", "12345PE",  neurologista)
-# adicionar à lista de medicos 
-neurologista.medicos.append(medico1)
 
+# Médico
+# Instâncias de Medico com atribuição de especialidade
+medico1 = Medico("Sophia Henrick", "12345PE",  neurologista)
+neurologista.medicos.append(medico1)# adiciona o médico à lista medicos em Especialidade
 medico2 = Medico("Renata Lee", "43210PE",  oncologista)
 oncologista.medicos.append(medico2)
-
 medico3 = Medico("Julio", "45678PE",  oncologista)
 oncologista.medicos.append(medico3)
-
 medico4 = Medico("Romero", "14785PE",  ortopedista)
 ortopedista.medicos.append(medico4)
-
 medico5 = Medico("Cesar", "36985PE",  oftamologista)
 ortopedista.medicos.append(medico5)
+
 # Paciente
-# Criação do paciente
+## Instancias de Paciente
 paciente1 = Paciente("123.456.789.1", "Monica Maria")
 paciente2 = Paciente("785.569.124.3", "Astrid Leonora")
 paciente3 = Paciente("456.159.357.2", "Carlos Fernandes")
 
-# !implementar a função de listar médico!
-print(medico1)
-print(medico2)
-print(medico3)
-print(medico4)
-print(medico5)
 
-# agendamento
+# Agendar consulta
+## Instancias de Consulta e agendamento de consultas
+print("Consultas agendadas")
+print("-" * 25)
+
 consulta1 = Consulta(medico1, neurologista, paciente2)
 consulta1.agendarConsulta()
+print("-" * 135)
 consulta2 = Consulta(medico4, ortopedista, paciente1)
 consulta2.agendarConsulta()
+print("-" * 135)
 consulta3 = Consulta(medico5, oftamologista, paciente3)
 consulta3.agendarConsulta()
+print("-" * 135)
 
-# cancelar consulta
+# Cancelar Consulta
 consulta1.cancelarConsulta(1)
+print("-" * 135)
 
+print("\nConsultas canceladas")
+print("-" * 35)
+cancelada = consulta2.listarCancelada()
+print(cancelada)
+
+print("\nBuscando uma consulta")
+print("-" * 35)
+busca2 = consulta2.buscarConsulta(2)
+print(busca2)
+print("-" * 135)
+
+"""
 for c in consultas:
     print(c)
     
@@ -118,3 +131,4 @@ print()
 
 for c in consultas:
     print(c)
+"""
